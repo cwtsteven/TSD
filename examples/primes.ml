403335f240d22845d44f5acbf980d77a99df9471
cwtsteven <cwtsteven@gmail.com>
2020-01-09 23:41:40 +0000

    modified primes example

--- examples/primes.ml
+++ examples/primes.ml
@@ -8,27 +8,21 @@ let (&&) = lift (&&)
 let fromn n = let s = cell (lift n) in s <~ [%dfg s + 1]; s 
 
 let inp = fromn 2 
+let filter = cell [%dfg true] 
 
-let out = cell inp
+let next () = 
+  step(); 
+  let new_filter = if peek filter then [%dfg inp mod (lift (peek inp - 1)) <> 0] else [%dfg true] in 
+  let old_filter = root filter in 
+  filter <~ [%dfg old_filter && new_filter]
 
-let filter = cell [%dfg true]
-
-let primes = [%dfg if filter then out else -1] 
-
-let next_prime () =  
-  let x = peek primes in 
-  (if (peek filter) 
-  then 
-    let new_filter = [%dfg inp mod (lift x) <> 0] in 
-    let old_filter = root filter in 
-    filter <~ [%dfg old_filter && new_filter] 
-  else 
-    ()
-  );
-  step();
-  x 
+let delay = 
+  let s = cell [%dfg -1] in 
+  s <~ inp; s
+let primes = [%dfg if filter then delay else -1] 
 
 let _ =
 	for i = 1 to 100 do
-		Printf.printf "%d\n" (next_prime());
-	done
+    next();
+		Printf.printf "%d\n" (peek primes);
+	done 
