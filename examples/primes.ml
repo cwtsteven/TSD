5314179bfa4604830d6653039f7d9a0733ccddcb
cwtsteven <cwtsteven@gmail.com>
2020-01-10 03:35:31 +0000

    modified primes example

--- examples/primes.ml
+++ examples/primes.ml
@@ -11,18 +11,18 @@ let fromn n =
   let s = cell (lift n) in 
   s <~ [%dfg s + 1]; s 
 
-let filter n = 
-  let i = fromn n in let n = lift n in 
-  [%dfg i == n || i mod n <> 0]
+let filter inp n = 
+  let n = lift n in 
+  [%dfg inp == n || inp mod n <> 0] 
 
 let inp = fromn 2 
-let sieve = cell (filter 2) 
+let sieve = cell (filter inp 2) 
 
 let next () = 
   step();
-  let newlayer = filter (peek inp) in
+  let newlayer = filter inp (peek inp) in
   let oldlayer = root sieve in 
-  sieve <~ [%dfg oldlayer && newlayer]
+  sieve <~ [%dfg oldlayer && newlayer] 
 
 let delay = cell inp 
 let primes = [%dfg if sieve then delay else -1] 
